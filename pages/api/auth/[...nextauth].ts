bdbbc1bee431e2d946e30d50368fa8e7c6382942
Carlos Martinez <curlosmart@gmail.com>
2023-06-11 17:06:48 -0400

    Add comments to "nextauth"

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,12 +9,23 @@ import AppleProvider from "next-auth/providers/apple";
 import { db } from "../../../firebase";
 import cryptoRandomString from 'crypto-random-string';
 
+/**
+ * @description
+ * @param {Session} session 
+ * @returns {DocumentReference<DocumentData>}
+ */
 const addNewUser = async (session: Session) => {
+  // Go into the database and attempt to find a user with the same username (or tag) as the one the current user is to trying to create an account with.
   const qUser = query(collection(db, "users"), where('tag', '==', session.user.tag));
+  // Using that queried user, get their docs.
   const qUserSnap = await getDocs(qUser);
 
+  // If the username that was entered does not exist, then use that username.
+  // Else if the username is already taken, then a random crypto string will be added to the end with a length of 6.
   const userTag = qUserSnap.docs.length === 0 ? session.user.tag : session.user.tag + cryptoRandomString({ length: 6 });
 
+  // TODO: Won't have to worry about email for now since I only have Google Auth setup but in the future will have to refactor when other auth methods are added.
+  // Add this user to the "users" collection, thus creating a NEW user.
   const docRef = await addDoc(collection(db, 'users'), {
     email: session.user.email,
     name: session.user.name,
@@ -27,11 +38,13 @@ const addNewUser = async (session: Session) => {
     dateJoined: serverTimestamp()
   });
 
+  // Return the docRef of the newly created user.
   return docRef;
 };
 
 export default NextAuth({
   // Configure one or more authentication providers
+  // TODO: Add CredentialsProvider (Need this to create a bunch of accounts without having to create a bunch of GMAIL accounts + pretty much every app lets you create an account without having to use a third-party source.)
   providers: [
     // CredentialsProvider({
     //   // The name to display on the sign in form (e.g. "Sign in with...")
@@ -83,7 +96,15 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
+    /**
+     * @description - This is callback function that'll be called after the AuthProvider is done dealing with the account given to it by the user (like a user signing up using their Google Account). Then we can get the base information about a user using the data from the AuthProvider. For example, a user signing in with a GMAIL account will have an email, a name, an image (for their icon), etc. so we'll get that information right at the start of the callback inside the "session.user" object.
+     * @param {
+     *  {Session}: session - The object with the information about the user
+     * } 
+     * @returns 
+     */
     async session({ session, token }) {
+      // TODO: No spaces allowed in the username and usernames are all set to all lowercase. This rule could change if I feel like it but for now I'll leave rule like this. It's not really a requirment but a preference.
       session.user.tag = session.user.name
         .split(" ")
         .join("")
@@ -93,8 +114,9 @@ export default NextAuth({
       const q = query(collection(db, "users"), where('email', '==', session.user.email));
       const querySnapshot = await getDocs(q);
 
+      // If user is signing in with an exisitng account, then set the keys in the object to the values that already exist in the database.
       if (querySnapshot.docs.length > 0) {
-        // If user is signing in with an exisitng account
+        // Get values from the database of this existing user and set it inside the session object
         const { name, tag, bio, location, website, dateJoined, profilePic, banner } = querySnapshot.docs[0].data();
 
         session.user.name = name;
@@ -108,7 +130,7 @@ export default NextAuth({
         session.user.banner = banner;
 
       } else {
-        // If user is signing up with a new account
+        // Else if user is signing up with a new account then we add them and create a new user using the information they or the AuthProvider (like Google) provided to us.
         const docRef = await addNewUser(session);
         session.user.uid = docRef.id;
 
@@ -121,11 +143,6 @@ export default NextAuth({
         session.user.website = website;
         session.user.dateJoined = dateJoined;
       }
-
-      console.log({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      });
 
       return session;
     },
