a01edcb1d00a0c52bb5f4a54069b2cfdb2d5de07
Carlos Martinez <curlosmart@gmail.com>
2025-09-13 10:39:45 -0400

    Add CLAUDE.md and remove unnecssary lock files from git.

--- firebase.ts
+++ firebase.ts
@@ -5,9 +5,6 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,16 +15,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: "${config.measurementId}"
 };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC-8BsNwn9Xnyz6I6Eu5P6DfHIpRSlgp8M",
-//   authDomain: "twitter-clone-ece43.firebaseapp.com",
-//   projectId: "twitter-clone-ece43",
-//   storageBucket: "twitter-clone-ece43.appspot.com",
-//   messagingSenderId: "108127960265",
-//   appId: "1:108127960265:web:675bd0177319866c996118",
-//   measurementId: "${config.measurementId}"
-// };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
