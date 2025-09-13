4c6afa5d9a0c473a6e1994bee7070698c228009a
Carlos Martinez <curlosmart@gmail.com>
2025-09-13 11:39:02 -0400

    FIX BUG: Spinner isn't being animated.

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 const Spinner = () => {
   return (
     <div className="flex items-center justify-center space-x-2">
-      <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+      <div className="animate-spin ease-linear rounded-full border-4 border-t-4 border-gray-200 border-t-lightblue-500 h-12 w-12 mb-4"></div>
     </div>
   );
 };
