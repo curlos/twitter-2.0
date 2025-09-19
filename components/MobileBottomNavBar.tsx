afcb9627fbd89be1814a4ac8f1fd25f97ed16cdc
Carlos Martinez <curlosmart@gmail.com>
2025-09-19 15:55:34 -0400

    MobileBottomNavbar - The 4 icons are not taking up an even amount of space - settings and dark/light mode taking up too much space.

--- components/MobileBottomNavBar.tsx
+++ components/MobileBottomNavBar.tsx
@@ -15,32 +15,38 @@ const MobileBottomNavBar: React.FC = () => {
   const [_sidenavOpen, setSidenavOpen] = useRecoilState(sidenavState);
 
   return (
-    <div className="sm:hidden fixed bottom-0 w-full border-t-[1px] bg-white border-[#AAB8C2] dark:bg-black  dark:border-gray-700 p-4 px-6 flex justify-between items-center h-[60px]">
+    <div className="sm:hidden fixed bottom-0 w-full border-t-[1px] bg-white border-[#AAB8C2] dark:bg-black  dark:border-gray-700 p-4 px-6 flex items-center h-[60px]">
       {/* Will redirect back home to the user's feed. */}
-      <Link href={`/`}>
-        <div>
-          <FaHome className="flex-1 h-6 w-6 cursor-pointer" />
-        </div>
-      </Link>
+      <div className="flex-1 flex justify-center">
+        <Link href={`/`}>
+          <FaHome className="h-6 w-6 cursor-pointer" />
+        </Link>
+      </div>
 
       {/* Clicking this will open the "Search" modal where users can type in whatever they want and filter the tweets shown by their query. */}
-      <FaSearch className="flex-1 h-6 w-6 cursor-pointer" onClick={() => setSearchModalOpen(true)} />
+      <div className="flex-1 flex justify-center">
+        <FaSearch className="h-6 w-6 cursor-pointer" onClick={() => setSearchModalOpen(true)} />
+      </div>
 
       {/* Clicking this will toggle between either 'light' mode or 'dark' mode depending on which mode you're currently on. */}
-      {theme === 'dark' ? (
-        <SunIcon className="flex-1 cursor-pointer h-[30px] w-[30px] dark:text-white" onClick={() => {
-          setTheme('light');
-          localStorage.theme = 'light';
-        }} />
-      ) : (
-        <MoonIcon className="flex-1 cursor-pointer h-[30px] w-[30px] dark:text-white" onClick={() => {
-          setTheme('dark');
-          localStorage.theme = 'dark';
-        }} />
-      )}
+      <div className="flex-1 flex justify-center">
+        {theme === 'dark' ? (
+          <SunIcon className="cursor-pointer h-[30px] w-[30px] dark:text-white" onClick={() => {
+            setTheme('light');
+            localStorage.theme = 'light';
+          }} />
+        ) : (
+          <MoonIcon className="cursor-pointer h-[30px] w-[30px] dark:text-white" onClick={() => {
+            setTheme('dark');
+            localStorage.theme = 'dark';
+          }} />
+        )}
+      </div>
 
       {/* This is the settings icon. When it's clicked, it will open the "Sidenav" so the user can see all the full detailed stuff that couldn't fit on this MobileBottomNavBar. */}
-      <CogIcon className="flex-1 h-7 w-7 cursor-pointer" onClick={() => setSidenavOpen(true)} />
+      <div className="flex-1 flex justify-center">
+        <CogIcon className="h-7 w-7 cursor-pointer" onClick={() => setSidenavOpen(true)} />
+      </div>
     </div>
   );
 };
