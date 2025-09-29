cf2197fd1aff609ada2956ae5908b74d4f18afb5
Carlos Martinez <curlosmart@gmail.com>
2025-09-29 15:14:21 -0400

    Tweet Version History - Don't show the parent tweet at the top of the page for quote tweets since that'll already show up inside the tweet component. Also, show an empty state message if the tweet hasn't been edited before.

--- pages/tweet/[id]/history/index.tsx
+++ pages/tweet/[id]/history/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AppLayout from '../../../../components/Layout/AppLayout';
 import PageHeader from '../../../../components/Layout/PageHeader';
 import ContentContainer from '../../../../components/Layout/ContentContainer';
-import { SparklesIcon } from '@heroicons/react/outline';
+import { SparklesIcon, ClockIcon } from '@heroicons/react/outline';
 import Tweet from '../../../../components/Tweet/Tweet';
 import DeletedTweet from '../../../../components/DeletedTweet';
 import { ITweet } from '../../../../utils/types';
@@ -10,7 +10,7 @@ import { useTweetData } from '../../../../hooks/useTweetData';
 
 const TweetVersionHistory = () => {
     // Use shared hook for tweet data (no isEditing needed for history page)
-    const { tweet, tweetID, author, replies, parentTweet, loading } = useTweetData(false, false);
+    const { tweet, tweetID, author, replies, parentTweet, loading, isQuoteTweet } = useTweetData(false, false);
 
     return (
         <AppLayout title={author ? `${author.name} on Twitter: "${tweet?.text}"` : "Tweet History / Twitter 2.0"}>
@@ -19,11 +19,11 @@ const TweetVersionHistory = () => {
                     <SparklesIcon className="h-5 w-5" />
                 </PageHeader>
 
-                {parentTweet && parentTweet.data() && (
+                {parentTweet && parentTweet.data() && !isQuoteTweet && (
                     <Tweet id={parentTweet.id} tweet={parentTweet.data()} tweetID={parentTweet.id} topParentTweet={true} />
                 )}
 
-                {parentTweet && !parentTweet.data() && (
+                {parentTweet && !parentTweet.data() && !isQuoteTweet && (
                     <DeletedTweet />
                 )}
 
@@ -32,16 +32,24 @@ const TweetVersionHistory = () => {
 
                 <div className="font-bold text-xl p-3 pt-5">Version History</div>
 
-                {/* List of Past Tweets */}
-                {tweet?.versionHistory && tweet?.versionHistory.toReversed().map((pastTweet: ITweet, index: number) => (
-                    <Tweet
-                        key={`${pastTweet.tweetId}-${pastTweet.timestamp?.seconds || index}`}
-                        id={String(pastTweet.tweetId)}
-                        tweet={pastTweet}
-                        tweetID={pastTweet.tweetId}
-                        pastTweet={true}
-                    />
-                ))}
+                {/* List of Past Tweets or Empty State */}
+                {tweet?.versionHistory && tweet?.versionHistory.length > 0 ? (
+                    tweet.versionHistory.toReversed().map((pastTweet: ITweet, index: number) => (
+                        <Tweet
+                            key={`${pastTweet.tweetId}-${pastTweet.timestamp?.seconds || index}`}
+                            id={String(pastTweet.tweetId)}
+                            tweet={pastTweet}
+                            tweetID={pastTweet.tweetId}
+                            pastTweet={true}
+                        />
+                    ))
+                ) : (
+                    <div className="flex flex-col items-center justify-center py-16 text-gray-900 dark:text-gray-500">
+                        <ClockIcon className="h-12 w-12 mb-4" />
+                        <p className="text-xl font-semibold">No edit history</p>
+                        <p className="text-gray-700 dark:text-gray-400 mt-2">This tweet hasn't been edited yet</p>
+                    </div>
+                )}
 
             </ContentContainer>
         </AppLayout>
